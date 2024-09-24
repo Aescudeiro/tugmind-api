3e5e174d5f80b1028632b5b9a5b23a5e6085fa38
Afonso Escudeiro <afonso.escudeiro@inspireit.pt>
2024-09-24 14:30:51 +0000

    added @nestjs/config and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TugMind API')
+    .setDescription('TugMind API documentation')
+    .setVersion('1.0')
+    .addTag('tugmind')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
